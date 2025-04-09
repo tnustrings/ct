ab7f3cab190640ee33ea32097bb3a13911397216
numlims <max.haubold@med.uni-greifswald.de>
2025-04-09 15:46:28 +0200

    write tex to file

--- ct/__main__.py
+++ ct/__main__.py
@@ -14,14 +14,14 @@ def main():
     # the .ct file
     parser.add_argument("ct_file", nargs="?", help="codetext file")
     # optional generated file
-    parser.add_argument("generated_file", nargs="?", help="generated file line number in ct", nargs="?") 
+    parser.add_argument("generated_file", nargs="?", help="generated file line number in ct") 
     # optional line number in generated file
     parser.add_argument("line_number", nargs="?", help="generated file line number")
     parser.add_argument("--tex", help="print doc as latex", action="store_true") 
     parser.add_argument("--from-org", help="input is a .org file", action="store_true")
     parser.add_argument("--mdtotex", nargs=1, help="for latex doc generation. a command to convert markdown between codechunks to tex, e.g. 'pandoc -f markdown -t latex'")
     parser.add_argument("--shell", action="store_true", help="run mdtotex command as shell script.")
-    parser.add_argument("-o", help="latex template out file, if run with --tex and without ct_file")
+    parser.add_argument("-o", help="out file for latex generation. run with --tex. if no ct file given, doc template is produced.")
     parser.add_argument("--header", help="latex template header file")
     parser.add_argument("--lower", action="store_true", help="lowercase tex template")
 
@@ -40,8 +40,8 @@ def main():
 
         return
 
-    f = args.ct_file
-    text = open(f).read()
+    f = open(args.ct_file)
+    text = f.read()
     f.close()
     
     # is text in org format? convert it to ct format
@@ -49,12 +49,21 @@ def main():
         text = org.orgtoct(text)
         # print(text)
         
-    # normal compilation
+    # involving ct compilation
     if args.generated_file is None:
         if args.tex == True:
             # run ct and print tex
             # args.mdtotex seems to be a string array
-            tex.printtex(text, mdtotex=args.mdtotex, shell=args.shell) # todo maybe print(tex.totex(text))
+            out = tex.totex(text, ctfile=args.ct_file, mdtotex=args.mdtotex, shell=args.shell) # todo maybe print(tex.totex(text))
+            # take the name for the out file from the ct file if none given
+            if args.o is None:
+                a = args.ct_file.split(".")
+                outname = a[0] + ".tex"
+            else:
+                outname = args.o
+            with open(outname, "w") as f:
+                f.write(out)
+                print("wrote " + outname)
         else:
             # run ct and write files
             ct.ctwrite(text)
